108eab627fc4c3737ac9cf4c810c56f920cbcdc7
norest <orestisef@gmail.com>
2018-12-02 14:41:31 +0200

    Reducers finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-import App from './components/App';
+import App from "./components/App";
+import reducers from "./reducers";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-   document.querySelector('#root')
- );
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
