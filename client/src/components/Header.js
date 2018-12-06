30ddbf3ceebf59f7880f799ecbd0f7e9585d2c87
norest <orestisef@gmail.com>
2018-12-06 19:07:38 +0200

    Redirect on Login-Logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a href= "/api/logout">Logout</a></li>;
+    }
+
+  }
+
+
   render() {
     return (
   <nav>
@@ -9,9 +26,7 @@ class Header extends Component {
       Emaily
       </a>
       <ul className="right">
-        <li>
-          <a>Login with Google</a>
-        </li>
+        {this.renderContent()}
       </ul>
     </div>
   </nav>
@@ -19,5 +34,8 @@ class Header extends Component {
   }
 }
 
+function mapStateToProps({ auth }){
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps) (Header);
