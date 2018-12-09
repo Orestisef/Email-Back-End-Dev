c4f1a32e269b756ffbe0030619153c1ffbd7a5b6
norest <orestisef@gmail.com>
2018-12-09 15:56:27 +0200

    Stripe Billing Process

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 class Header extends Component {
@@ -22,9 +23,12 @@ class Header extends Component {
     return (
   <nav>
     <div className="nav-wrapper">
-      <a className="brand-logo left">
+      <Link
+      to={this.props.auth ? '/surveys' : '/'}
+      className="brand-logo left"
+      >
       Emaily
-      </a>
+      </Link>
       <ul className="right">
         {this.renderContent()}
       </ul>
